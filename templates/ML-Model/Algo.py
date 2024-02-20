c1c0aab9a2e4699e1f8f225cdd1123e594e59c8b
RjSup <19141558@brookes.ac.uk>
2024-02-20 12:50:29 +0000

    update algo

--- templates/ML-Model/Algo.py
+++ templates/ML-Model/Algo.py
@@ -82,7 +82,7 @@ def algo(healthy_images, broken_images):
     feature_vectors, training_labels = training_data
 
     # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(feature_vectors, training_labels, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(feature_vectors, training_labels, test_size=0.2, random_state=42, shuffle=True)
 
     # Initialize the decision tree classifier
     clf = DecisionTreeClassifier()
